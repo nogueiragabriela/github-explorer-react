7d99581de73f0ad8339ac573060091f7edddc0a3
nogueiragabriela <nogueiragabriela93@gmail.com>
2022-11-13 14:34:32 -0300

    Criando o GitHub Explorer

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,12 @@
+export function RepositoryItem() {
+    return (
+        <li>
+            <strong>unform</strong>
+            <p>Forms in React </p>
+
+            <a href="">
+                Acessa repositorio
+            </a>
+        </li>
+    );
+}
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import { render } from 'react-dom';
+//import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+render(<App />, document.getElementById('root'));
+//root.render(<App />, document.getElementById('root'))
