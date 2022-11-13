d38a9483f7b4fb838bba19fab0a700afeaa4b638
nogueiragabriela <nogueiragabriela93@gmail.com>
2022-11-13 16:32:43 -0300

    Aplicando useEffect

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -5,7 +5,7 @@ export function RepositoryItem(props) {
             <p>{props.repository?.description} </p>
 
             <a href={props.repository?.link}>
-                Acessa repositorio
+                Acessa repositório
             </a>
         </li>
     );
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+
 
 const repository = {
     name: 'unform',
@@ -9,9 +11,20 @@ const repository = {
 }
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []);
+
+
+
+
     return (
         <section className="repository-list">
-            <h1>Lista de Repositorios</h1>
+            <h1>Lista de Repositórios</h1>
 
             <ul>
                 <RepositoryItem repository={repository} />
