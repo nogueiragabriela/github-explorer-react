04144dbe91032e8162e055a5f6587ca03daeb270
nogueiragabriela <nogueiragabriela93@gmail.com>
2022-11-13 17:40:41 -0300

    Componentes de .jsx para .tsx

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,11 +1,19 @@
-export function RepositoryItem(props) {
+interface RepositoryItemProps {
+    repository: {
+        name: string;
+        description: string;
+        html_url: string;
+    }
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
     return (
         <li>
             <strong>{props.repository?.name ?? 'Default'}</strong>
             <p>{props.repository?.description} </p>
 
             <a href={props.repository?.html_url}>
-                Acessa repositório
+                Acessa o repositório
             </a>
         </li>
     );
